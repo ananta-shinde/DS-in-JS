fa9c6e5d24de7cd2fbf2d2a47d4e859415a742a0
aShinde <ananta.shinde@honeywellcom>
2024-04-03 12:00:40 +0530

    03/04/24

--- Linkedlist.js/singleLinkedList.js
+++ Linkedlist.js/singleLinkedList.js
@@ -32,17 +32,62 @@ class SignlyLinkedList
             temp = temp.next
         }
     }
+
+    findByValue = (value)=>{
+          var temp = this.head;
+          while(temp != null){
+            if(temp.data == value)
+            {
+                return temp.data;
+            }
+            temp = temp.next
+          }
+          return null;
+    }
+
+    deleteByValue = (value)=>{
+        var temp = this.head;
+        while(temp.next.data != value)
+        {
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+    }
+
+    size = ()=>{
+        var count = 0;
+        if(this.head == null){
+            return 0;
+        }else{
+             var temp = this.head;
+             while(temp !=null)
+             {
+                count++
+                temp = temp.next
+             }
+        }
+        return count;
+    }
+
+
 }
 
 
 var slist = new SignlyLinkedList();
 
 slist.push(20);
+
 slist.push(40);
 slist.push(50);
 slist.push(60);
+
 slist.push(50);
 slist.push(80);
+
+slist.printList();
+console.log(slist.findByValue(160));
+slist.deleteByValue(50);
+console.log(slist.size())
 slist.printList();
 
 
